ca87c868260cdc28b1a118397d7c4f57d127c796
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-12-12 10:10:04 -0800

    section 3 done

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const redSection = gsap.timeline({
         // markers: true
     }
 })
-const greenSection = gsap.timeline()
+const greenSection = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".section3",
+        start: "top 10%",
+        markers: true
+    }
+})
 const lastSection = gsap.timeline()
 
 blueSection
@@ -20,3 +26,8 @@ blueSection
 redSection
   .from('.section2 h2 span', {y: "-100vh", stagger: .3, duration: .5})
   .from('.section2 p', {duration: 1, rotationY: 90, opacity: 0});
+
+greenSection
+  .from('.section3 div h2', {x: "100vw", scale: 20, duration: .8})
+  .from('.section3 div p span', {backgroundColor: "white", y: "100vh", stagger: .2, duration: 1})
+  .to('.section3 div p span', {backgroundColor: "white", color: "black", stagger:.2, duration: 1}, "<")
