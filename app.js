7f2c0a3d38ad6e417f025086a4e2080bfe682a45
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-12-12 09:54:48 -0800

    section 2 done

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const blueSection = gsap.timeline()
-const redSection = gsap.timeline()
+const redSection = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".section2",
+        start: "top 20%",
+        // markers: true
+    }
+})
 const greenSection = gsap.timeline()
 const lastSection = gsap.timeline()
 
 blueSection
   .from('.content div h1 span:last-child', {x: "100vw", duration: 1})
   .from('.content div h1 span:first-child', {x: "-100vw", duration: 1}, "<")
-  .from('.content div p', {transformOrigin: "left", rotation: 90, opacity: 0, duration: .5})
+  .from('.content div p', {transformOrigin: "left", rotation: "90deg", opacity: 0, duration: .5})
   .from('.content img', {y: "100vh", duration: .5})
+
+redSection
+  .from('.section2 h2 span', {y: "-100vh", stagger: .3, duration: .5})
+  .from('.section2 p', {duration: 1, rotationY: 90, opacity: 0});
