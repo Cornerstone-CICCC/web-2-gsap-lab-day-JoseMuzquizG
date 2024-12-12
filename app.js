3869f1f51ef04465ae38ff5d682449a72687d8ad
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-12-12 09:27:28 -0800

    section 1 done

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// YOUR CODE HERE
+const blueSection = gsap.timeline()
+const redSection = gsap.timeline()
+const greenSection = gsap.timeline()
+const lastSection = gsap.timeline()
+
+blueSection
+  .from('.content div h1 span:last-child', {x: "100vw", duration: 1})
+  .from('.content div h1 span:first-child', {x: "-100vw", duration: 1}, "<")
+  .from('.content div p', {transformOrigin: "left", rotation: 90, opacity: 0, duration: .5})
+  .from('.content img', {y: "100vh", duration: .5})
