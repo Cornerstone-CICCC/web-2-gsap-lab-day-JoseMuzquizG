ba0b4672e183f21ccb062503ec4820b548f1fef9
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-12-13 09:10:41 -0800

    tried doing lateral slide

--- app.js
+++ app.js
@@ -5,17 +5,39 @@ const redSection = gsap.timeline({
     scrollTrigger: {
         trigger: ".section2",
         start: "top 20%",
-        // markers: true
+        // markers: true,
+        toggleActions: "play none none reverse" 
     }
 })
 const greenSection = gsap.timeline({
     scrollTrigger: {
         trigger: ".section3",
         start: "top 10%",
-        markers: true
+        // markers: true,
+        toggleActions: "play none none reverse" 
     }
 })
-const lastSection = gsap.timeline()
+
+const purpleSection = gsap.timeline({
+    // scrollTrigger: {
+    //     trigger: ".section3",
+    //     start: "top top",
+    //     pin: true,
+    //     scrub: true,
+    //     markers: true,
+    //     toggleActions: "play none none reverse" 
+    // }
+})
+
+const lastSection = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".section5",
+        start: "top top%",
+        scrub: true,
+        // markers: true,
+        toggleActions: "play none none reverse" 
+    }
+})
 
 blueSection
   .from('.content div h1 span:last-child', {x: "100vw", duration: 1})
@@ -31,3 +53,26 @@ greenSection
   .from('.section3 div h2', {x: "100vw", scale: 20, duration: .8})
   .from('.section3 div p span', {backgroundColor: "white", y: "100vh", stagger: .2, duration: 1})
   .to('.section3 div p span', {backgroundColor: "white", color: "black", stagger:.2, duration: 1}, "<")
+
+purpleSection
+
+
+
+let contact = document.querySelector(".section5 div h2")
+let text = contact.innerHTML
+let splitText = text.split("")
+
+let letters = splitText.forEach((letter) => {
+    const newLetter = document.createElement("span")
+    newLetter.innerHTML = letter
+}) 
+
+console.log(letters)
+
+lastSection
+  .from(".section5 div h2", {y: "-100vh", duration: 1})
+  .from('.section5 div p', {y: 30, opacity: 0, duration: .5})
+  .from('.section5 div button', {scale: 0, duration: 0.5})
+//   .from(".section5", {backgroundPositionX: "100vw", duration: 2})
+
+
